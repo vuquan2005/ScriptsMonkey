d9925ae8c0994f57e014ad4acdc01d23bda609a3
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-04-15 20:55:05 +0700

    Update EOP_Bot.js

--- EOP_Bot.js
+++ EOP_Bot.js
@@ -4,7 +4,7 @@
 // @version      0.3.3
 // @description  A bot working on eop.edu.vn
 // @author       QuanVu
-// @include      https://eop.edu.vn/study/*
+// @include      https://eop.edu.vn/study/task/*
 // @updateURL    https://raw.githubusercontent.com/vuquan2005/ScriptsMonkey/refs/heads/main/EOP_Bot.js
 // @downloadURL  https://raw.githubusercontent.com/vuquan2005/ScriptsMonkey/refs/heads/main/EOP_Bot.js
 // @grant        none
