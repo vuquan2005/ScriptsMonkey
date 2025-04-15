39b66416a9409c42c4925085633d0ff8b2cb6f14
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-04-15 20:37:13 +0700

    Create EOP_Bot.js

---
+++ EOP_Bot.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         EOP Bot
+// @namespace    https://github.com/vuquan2005
+// @version      0.3.3
+// @description  A bot working on eop.edu.vn
+// @author       QuanVu
+// @include      https://eop.edu.vn/study/*
+// @updateURL    https://raw.githubusercontent.com/vuquan2005/ScriptsMonkey/refs/heads/main/EOP_Bot.js
+// @downloadURL  https://raw.githubusercontent.com/vuquan2005/ScriptsMonkey/refs/heads/main/EOP_Bot.js
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    
+    //End
+})();
