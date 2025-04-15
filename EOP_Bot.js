09c985471093684eda708bfc39d8a0617b65bd96
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-04-15 23:54:20 +0700

    Update EOP bot to 3.1

--- EOP_Bot.js
+++ EOP_Bot.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EOP Bot
 // @namespace    https://github.com/vuquan2005
-// @version      3.0
+// @version      3.1
 // @description  A bot working on eop.edu.vn
 // @author       QuanVu
 // @include      https://eop.edu.vn/study/task/*
@@ -13,9 +13,9 @@
 
 (function () {
     "use strict";
-    //
-    var taskTitleElement = document.querySelector("span#dtasktitle");
-    var taskTitle = '';
+
+    const taskTitleElement = document.querySelector("span#dtasktitle");
+    let taskTitle = "";
 
     setInterval(() => {
         if (taskTitleElement.textContent !== taskTitle) {
@@ -28,61 +28,86 @@
         console.log("\nNew Task");
         // Định nghĩa các phần tử và hàm xử lý tương ứng
         const elementHandlers = [
-            { selector: 'div.dvocabulary', handler: handleVocab },
-            { selector: 'div.dmcq', handler: handleDMCQ },
-            { selector: 'div.dquestion', handler: handleQuestion },
-            { selector: 'div.dcontent', handler: handleContent },
+            { selector: "div.dvocabulary", handler: handleVocab },
+            { selector: "div.dmcq", handler: handleDMCQ },
+            { selector: "div.dquestion", handler: handleQuestion },
+            { selector: "div.dcontent", handler: handleContent },
         ];
 
         // Duyệt và xử lý phần tử tương ứng
         elementHandlers.forEach(({ selector, handler }) => {
             const el = document.querySelector(selector);
             if (el) {
-                console.log('Tìm thấy phần tử: ', selector);
+                console.log("Tìm thấy phần tử: ", selector);
                 handler(el);
             }
         });
 
         // Các hàm xử lý tương ứng
         function handleVocab(el) {
-            console.log('Handling vocabulary...');
-            console.log('Can\'t do this, because the EOP detect:\n\"Uncaught (in promise) NotAllowedError: play() failed because the user didn\'t interact with the document first.\"');
+            console.log("Handling vocabulary...");
+            console.log(
+                "Can't do this, because the EOP detect:\n\"Uncaught (in promise) NotAllowedError: play() failed because the user didn't interact with the document first.\""
+            );
+            // el.querySelectorAll('i.daudio').forEach((input) => {
+            //     if (input) {
+            //         setInterval(() => {
+            //             input.click();
+            //             input.log('Playing audio...');
+            //         }, 3000);
+            //     }
+            // });
         }
 
         function handleDMCQ(el) {
-            console.log('Handling DMCQ...');
-            console.log('Can\'t do this, because i think so');
+            console.log("Handling DMCQ...");
+            console.log("Can't do this, because i think so");
         }
 
         function handleQuestion(el) {
-            console.log('Handling question...');
+            console.log("Handling question...");
             const chooseQuestion = el.querySelector("p.dchk");
             if (chooseQuestion) {
-                console.log('Can\'t do the choose question');
-            }
-            else {
-                console.log('Bot do');
+                console.log("Can't do the choose question");
+            } else {
+                console.log("Bot do");
                 // Call questionFill() in "scripts/questionFill.js"
+                var timeDoTask = TimeDoTask();
+                console.log("Task will be completed in: ", timeDoTask + 30, " s");
                 questionFill();
             }
         }
 
         function handleContent(el) {
-            console.log('Handling content...');
+            console.log("Handling content...");
             setInterval(() => {
-                const btnDone = document.querySelector('button.btn.btn-info.dnut[type="button"]');
-            if (btnDone) {
-                btnDone.click();
-            }
+                const btnDone = document.querySelector(
+                    'button.btn.btn-info.dnut[type="button"]'
+                );
+                if (btnDone) {
+                    btnDone.click();
+                }
             }, 5000);
         }
-
     }
 
     function delay(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
 
+    function TimeDoTask() {
+        const contentElement = document.querySelector("div.ditem");
+        const listenQuestion = document.querySelector("div.dta-main");     
+        if (contentElement) {
+            const text = contentElement.textContent;
+            const wordMatchRegExp = /[^\s]+/g;
+            const words = text.matchAll(wordMatchRegExp);
+            const wordCount = [...words].length;
+            let readingTime = (wordCount / 300)*60;
+            if (listenQuestion) readingTime += 90;
+            return readingTime;
+        }
+    }
     async function questionFill() {
         // Fill in the answers
         await document.querySelectorAll("input").forEach((input) => {
@@ -90,20 +115,20 @@
                 input.value = "a";
             }
         });
-    
+
         // Click the Done buttons
         const btnDone = document.querySelector('button.btn.btn-info.dnut[type="button"]');
         if (btnDone) {
             btnDone.click();
         }
-    
+
         // Create a button inside <div id="mfooter">
         const mfooter = document.getElementById("mfooter");
         const clockBtn = document.createElement("button");
         clockBtn.className = "btn dnut";
         clockBtn.style = "display: inline-block;";
         mfooter.appendChild(clockBtn);
-    
+
         // Find the Preview button and click it
         const btnPreview = document.querySelector(
             'button.btn.btn-danger.dnut[type="button"]'
@@ -117,7 +142,7 @@
                 clockBtn.textContent = `Xem đáp án sau: ${countdown}`;
                 if (countdown <= 0) {
                     clearInterval(countdownInterval);
-    
+
                     btnPreview.click();
                     clockBtn.textContent = "Đã có đáp án";
                 }
@@ -139,9 +164,9 @@
             .catch((err) => {
                 alert("Lỗi trong quá trình nhận diện:", err);
             });
-    
+
         console.log(answersTxt);
-    
+
         await delay(100);
         const btnReW = document.querySelector(
             'button.btn.btn-primary.dnut[type="button"]'
@@ -149,7 +174,7 @@
         if (btnReW) {
             btnReW.click();
         }
-    
+
         // Fill in the recognized text into the input fields
         const inputFields = document.querySelectorAll("input[type='text']");
         inputFields.forEach((input, index) => {
@@ -157,11 +182,10 @@
                 input.value = answersTxt[index];
             }
         });
-        //random 2-30s
-    
-        let random = Math.floor(Math.random() * 30000) + 2000;
-        clockBtn.textContent = `Đã điền xong, chờ ${random.toFixed(1) / 1000}s`;
-        await delay(random);
+
+        let timeDo = Math.round(TimeDoTask());
+        clockBtn.textContent = 'Đã điền xong, chờ ' + timeDo + 's';
+        await delay(timeDo * 1000);
         // Click the Done buttons
         const btnDone2 = document.querySelector(
             'button.btn.btn-info.dnut[type="button"]'
@@ -169,10 +193,10 @@
         if (btnDone2) {
             btnDone2.click();
         }
-    
+
         //end
     }
-    
+
     function getAnswer() {
         const inputs = document.querySelectorAll(
             'input[style*="background-image"]'
@@ -185,10 +209,10 @@
         });
         return imageArray;
     }
-    
+
     async function imgToTxt(images) {
         const results = [];
-    
+
         for (const imagePath of images) {
             try {
                 const {
@@ -204,8 +228,8 @@
                 console.error("Error recognizing image:", err);
             }
         }
-    
+
         return results;
-    }    
+    }
     //End
 })();
