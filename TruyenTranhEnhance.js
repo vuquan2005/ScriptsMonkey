c77dc4ea0bdb30c7391102af4c9cf6f9275e9811
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-04-15 18:31:51 +0700

    Create TruyenTranhEnhance.js

---
+++ TruyenTranhEnhance.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         TruyenTranhEnhance
+// @namespace    https://github.com/vuquan2005
+// @version      0.0.1
+// @description  Enhance your Manga reading experience
+// @author       QuanVu
+// @include      /^https:\/\/goctruyentranhvui\d+\.com\/.*$/
+// @updateURL
+// @downloadURL
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+})();
