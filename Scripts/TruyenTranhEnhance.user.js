1dd3a55a654d80019b1f055f1f9865ea8df3707c
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-04-26 23:34:34 +0700

    Scripts/TruyenTranhEnhance.user.js, clean code

--- Scripts/TruyenTranhEnhance.user.js
+++ Scripts/TruyenTranhEnhance.user.js
@@ -14,113 +14,128 @@
 
 (function () {
     "use strict";
-    //Scroll
-    let clickCount = 0;
-    let clickTimer = null;
-    let longPressTimer = null;
-    let isLongPress = false;
-    const delay = 250;
-    const longPressDelay = 500;
-    let truyen = document.querySelector("div.image-section");
-    truyen.onmousedown = function () {
-        isLongPress = false;
-        longPressTimer = setTimeout(() => {
-            console.log("Long Press detected");
-            isLongPress = true;
-            clickCount = 0;
-        }, longPressDelay);
-    };
-    truyen.onmouseup = function () {
-        clearTimeout(longPressTimer);
-        if (isLongPress) {
-            return;
-        }
-        clickCount++;
-        if (clickCount === 1) {
-            clickTimer = setTimeout(() => {
-                // Single click
-                const scrollY = window.innerHeight * 0.55;
-                window.scrollBy({
-                    top: scrollY,
-                    behavior: "smooth",
-                });
+    const $ = (selector, scope = document) => scope.querySelector(selector);
+    const $$ = (selector, scope = document) => scope.querySelectorAll(selector);
+    // ==================================
+    // Scroll
+    const Enhance_Scroll = function () {
+        let clickCount = 0;
+        let clickTimer = null;
+        let longPressTimer = null;
+        let isLongPress = false;
+        const delay = 200;
+        const longPressDelay = 400;
+        let truyen = $("div.image-section");
+        let tabNavigator = $("div.top-move-pannel");
+        // Event mosuse down
+        truyen.onmousedown = function () {
+            isLongPress = false;
+            longPressTimer = setTimeout(() => {
+                // Long press
+                tabNavigator.classList.toggle("fixed-toggle");
+                isLongPress = true;
                 clickCount = 0;
-            }, delay);
-        } else if (clickCount === 2) {
-            clearTimeout(clickTimer);
-            // Double click
-            clickCount = 0;
-        } else if (clickCount === 3) {
-            // Triple click
-            clickCount = 0;
-        }
-    };
-    // Change opacity
-    let opacity = GM_getValue("opacity", 1);
-    console.log(opacity);
-    GM_addStyle(`
-        img.image.finished { opacity: ${opacity};}
-        .opacity-btn {
-            position: fixed;
-            left: 0px;
-            bottom: 0px;
-            width: auto;
-            height: auto;
-            border-radius: 10px;
-            display: flex;
-            z-index: 1000;
-        }
-        .opacity-btn1, .opacity-btn2 {
-            color:#ffffffee;
-            display: flex;
-            background-color:rgba(0, 0, 0, 0.1);
-            margin: 2px;
-            padding: 5px;
-            font-size: 15px;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
+            }, longPressDelay);
+        };
+        // Event mouse up
+        truyen.onmouseup = function () {
+            clearTimeout(longPressTimer);
+            if (isLongPress) {
+                return;
             }
-    `);
-    let body = document.querySelector("div.setting");
-    let opacityButton = `
-        <div class="opacity-btn">
-            <button class="opacity-btn1">+</button>
-            <button class="opacity-btn2">-</button>
-        </div>
-    `;
-    body.insertAdjacentHTML("beforebegin", opacityButton);
-    //
-    let images = document.querySelectorAll("img.image.finished");
-    let opacityButton1 = document.querySelector("button.opacity-btn1");
-    opacityButton1.addEventListener("click", function () {
-        if (opacity >= 1) {
-            opacity = 1;
-            console.log(opacity);
-            return;
-        }
-        opacity += 0.1;
-        GM_setValue("opacity", opacity);
+            clickCount++;
+            if (clickCount === 1) {
+                clickTimer = setTimeout(() => {
+                    // Single click
+                    const scrollY = window.innerHeight * 0.55;
+                    window.scrollBy({
+                        top: scrollY,
+                        behavior: "smooth",
+                    });
+                    clickCount = 0;
+                }, delay);
+            } else if (clickCount === 2) {
+                clearTimeout(clickTimer);
+                // Double click
+                clickCount = 0;
+            } else if (clickCount === 3) {
+                // Triple click
+                clickCount = 0;
+            }
+        };
+    };
+    // ==================================
+    // Change Opacity
+    const Chhange_Opacity = function () {
+        let opacity = GM_getValue("opacity", 1);
         console.log(opacity);
         GM_addStyle(`
             img.image.finished { opacity: ${opacity};}
-            });
+            .opacity-btn {
+                position: fixed;
+                left: 0px;
+                bottom: 0px;
+                width: auto;
+                height: auto;
+                border-radius: 10px;
+                display: flex;
+                z-index: 1000;
+            }
+            .opacity-btn1, .opacity-btn2 {
+                color:#ffffffee;
+                display: flex;
+                background-color:rgba(0, 0, 0, 0.1);
+                margin: 2px;
+                padding: 5px;
+                font-size: 15px;
+                border: none;
+                border-radius: 10px;
+                cursor: pointer;
+                }
         `);
-    });
-    let opacityButton2 = document.querySelector("button.opacity-btn2");
-    opacityButton2.addEventListener("click", function () {
-        if (opacity <= 0) {
-            opacity = 0;
+        let divSetting = $("div.setting");
+        let divOpacityButton = `
+            <div class="opacity-btn">
+                <button class="opacity-btn1">+</button>
+                <button class="opacity-btn2">-</button>
+            </div>
+        `;
+        divSetting.insertAdjacentHTML("beforebegin", divOpacityButton);
+        //
+        //let images = $$("img.image.finished");
+        let btnIncreaseOpacity = $("button.opacity-btn1");
+        btnIncreaseOpacity.addEventListener("click", function () {
+            if (opacity >= 1) {
+                opacity = 1;
+                console.log(opacity);
+                return;
+            }
+            opacity += 0.1;
+            GM_setValue("opacity", opacity);
             console.log(opacity);
-            return;
-        }
-        opacity -= 0.1;
-        GM_setValue("opacity", opacity);
-        console.log(opacity);
-        GM_addStyle(`
-            img.image.finished { opacity: ${opacity};}
-            });
-        `);
-    });
-    //End
+            GM_addStyle(`
+                img.image.finished { opacity: ${opacity};}
+                });
+            `);
+        });
+        let btnDecreaseOpacity = $("button.opacity-btn2");
+        btnDecreaseOpacity.addEventListener("click", function () {
+            if (opacity <= 0) {
+                opacity = 0;
+                console.log(opacity);
+                return;
+            }
+            opacity -= 0.1;
+            GM_setValue("opacity", opacity);
+            console.log(opacity);
+            GM_addStyle(`
+                img.image.finished { opacity: ${opacity};}
+                });
+            `);
+        });
+    };
+    // ==================================
+    Enhance_Scroll();
+    Chhange_Opacity();
+    // End Script
 })();
