bf0489991a1efcebcd1e85920e89e86cd11eafa5
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-04-28 21:45:34 +0700

    Scripts/TruyenTranhEnhance.user.js

--- Scripts/TruyenTranhEnhance.user.js
+++ Scripts/TruyenTranhEnhance.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GocTruyenTranhEnhance
 // @namespace    https://github.com/vuquan2005/ScriptsMonkey
-// @version      2.7.0
+// @version      2.7.1
 // @description  Enhance your Manga reading experience
 // @author       QuanVu
 // @include      /https:\/\/goctruyentranhvui\d+\.com\/truyen\/.*/
