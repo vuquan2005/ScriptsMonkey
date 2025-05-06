d7f6c85c77cecb491fdf205bbf8fda049b431e16
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-06 20:21:08 +0700

    Update svHaUI_Helper.user.js

--- Scripts/svHaUI_Helper.user.js
+++ Scripts/svHaUI_Helper.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         sv.HaUI
 // @namespace    https://github.com/vuquan2005/ScriptsMonkey
-// @version      2.1
+// @version      2.2
 // @description  Do some things on sv.haui.edu.vn
 // @author       QuanVu
-// @downloadURL  https://github.com/vuquan2005/ScriptsMonkey/raw/main/Scripts/Change_header_svHaUI.user.js
-// @updateURL  https://github.com/vuquan2005/ScriptsMonkey/raw/main/Scripts/Change_header_svHaUI.userss.js
+// @downloadURL  https://github.com/vuquan2005/ScriptsMonkey/raw/main/Scripts/svHaUI_Helper.user.js
+// @updateURL  https://github.com/vuquan2005/ScriptsMonkey/raw/main/Scripts/svHaUI_Helper.user.js
 // @match        https://sv.haui.edu.vn/*
 // @grant        none
 // ==/UserScript==
@@ -13,10 +13,12 @@
 (function () {
     "use strict";
     // Change header
-    const newTitle = document.querySelector("div.panel-heading").textContent;
-    document.title = newTitle;
-    // add Học kết hợp! tab to sidebar
-    const tabToAddElement = document.querySelector('ul.sidebar-elements > li:nth-child(2)');
+    setTimeout(() => {
+        const newTitle = document.querySelector("div.panel-heading").textContent;
+        document.title = newTitle;
+    }, 700);
+    // add Học kết hợp to sidebar
+    const tabToAddElement = document.querySelector("ul.sidebar-elements > li:nth-child(2)");
     if (tabToAddElement) {
         let studyTabNext = `
             <li>
