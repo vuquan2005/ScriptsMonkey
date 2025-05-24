57581c31b4d7b02c71793cf687736d9c990bfb42
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-24 19:47:47 +0700

    Update svHaUI_Helper.user.js

--- Scripts/svHaUI_Helper.user.js
+++ Scripts/svHaUI_Helper.user.js
@@ -1,23 +1,45 @@
 // ==UserScript==
 // @name         sv.HaUI
 // @namespace    https://github.com/vuquan2005/ScriptsMonkey
-// @version      2.4
+// @version      2.5
 // @description  Công cụ hỗ trợ cho sinh viên HaUI
 // @author       QuanVu
 // @downloadURL  https://github.com/vuquan2005/ScriptsMonkey/raw/main/Scripts/svHaUI_Helper.user.js
 // @updateURL    https://github.com/vuquan2005/ScriptsMonkey/raw/main/Scripts/svHaUI_Helper.user.js
 // @match        https://sv.haui.edu.vn/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
     "use strict";
     // =====================================================================================
+    const currentURL = window.location.href;
+    const $ = (selector, scope = document) => scope.querySelector(selector);
+    const $$ = (selector, scope = document) => scope.querySelectorAll(selector);
+    function controlInterval(func, delayDefault = 1000) {
+        let intervalId = null;
+        return {
+            start: (delay = delayDefault, startImmediate = false) => {
+                if (intervalId) {
+                    clearInterval(intervalId);
+                }
+                intervalId = setInterval(func, delay);
+                if (startImmediate) {
+                    func();
+                }
+            },
+            stop: () => {
+                clearInterval(intervalId);
+                intervalId = null;
+            },
+        };
+    }
+    // =====================================================================================
     // Change header
     function changeHeader() {
-        if (!document.querySelector("div.panel-heading")) return;
-        let newTitle = document.querySelector("div.panel-heading").textContent;
-        console.log("Last title: ", newTitle);
+        if (!$("div.panel-heading")) return;
+        let newTitle = $("div.panel-heading").textContent;
+        //console.log("Last title: ", newTitle);
         newTitle = newTitle.replace("\n", "");
         newTitle = newTitle.replace("\t", "");
         newTitle = newTitle.replace("TRƯỜNG ĐẠI HỌC CÔNG NGHIỆP HÀ NỘI", "");
@@ -27,38 +49,16 @@
     }
     // add Học kết hợp to sidebar
     function addStudyTab() {
-        const beLeftSidebar = document.querySelector("div.be-left-sidebar");
+        if (currentURL != "https://sv.haui.edu.vn/") {
+            return;
+        }
+        const beLeftSidebar = $("div.be-left-sidebar");
         if (beLeftSidebar) {
             let studyTabNext = `
                 <div class="be-content">
                     <div class="main-content container-fluid">
                         <form name="frmMain" id="frmMain" data-toggle="validator" role="form">
                             <div class="panel panel-default panel-border-color panel-border-color-primary">
-                                <style>
-                                    #add-them-tab {
-                                        display: block;
-                                    }
-                                    #add-them-tab > p {
-                                        font-size: 20px;
-                                        margin: 10px;
-                                        padding: 10px;
-                                        color: #3d3d3d;
-                                        border-radius: 10px;
-                                        border: 1px solid #3d3d3d;
-                                        line-height: 2;
-                                    }
-                                    #add-them-tab > p > a {
-                                        color: #3d3d3d;
-                                    }
-                                    #add-them-tab > p > a:hover {
-                                        background-color: #d0f0db;
-                                        color: #000000;
-                                    }
-                                    #add-them-tab > p > a > i {
-                                        margin-right: 10px;
-                                        scale: 1.5;
-                                    }
-                                </style>
                                 <div id="add-them-tab">
                                     <p>Một số chức năng chính</p>
                                     <p>
@@ -107,14 +107,40 @@
                 </div>
             `;
             beLeftSidebar.insertAdjacentHTML("afterend", studyTabNext);
+            GM_addStyle(`
+                #add-them-tab {
+                    display: block;
+                }
+                #add-them-tab > p {
+                    font-size: 20px;
+                    margin: 10px;
+                    padding: 10px;
+                    color: #3d3d3d;
+                    border-radius: 10px;
+                    border: 1px solid #3d3d3d;
+                    line-height: 2;
+                }
+                #add-them-tab > p > a {
+                    color: #3d3d3d;
+                }
+                #add-them-tab > p > a:hover {
+                    background-color: #d0f0db;
+                    color: #000000;
+                }
+                #add-them-tab > p > a > i {
+                    margin-right: 10px;
+                    scale: 1.5;
+                }
+                `);
         }
     }
     // ======================================================================================
-    changeHeader();
-    if (window.location.href == "https://sv.haui.edu.vn/") {
+    const changeHeaderInterval = controlInterval(changeHeader, 5000);
+    setTimeout(() => {
+        // Run
+        console.log("sv.HaUI loaded: ");
+        changeHeaderInterval.start(5000, true);
         addStudyTab();
-    }
-    setInterval(() => {
-        changeHeader();
-    }, 10000);
+        highlightScores();
+    }, 50);
 })();
