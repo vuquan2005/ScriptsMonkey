cdd5a1d45fe83361f8476275748c6936567f86fe
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-06-02 00:59:27 +0700

    Increment version to 0.1.2

--- Scripts/Save_the_multiple_questions.user.js
+++ Scripts/Save_the_multiple_questions.user.js
@@ -3,7 +3,7 @@
 // @description		Lưu lại câu hỏi trắc nghiệm trên hệ thống quản lý học tập qldt.haui.edu.vn
 // @author         	QuanVu
 // @namespace      	https://github.com/vuquan2005/ScriptsMonkey
-// @version        	0.1.1
+// @version        	0.1.2
 // @match          	https://qlht.haui.edu.vn/mod/quiz/attempt.php*
 // @match          	https://qlht.haui.edu.vn/mod/quiz/summary.php*
 // @grant          	GM_setValue
